5415b98f1bdd47a8af1d452ebbd9d876982e4658
Shraddha Sharma <shr.comp1@gmail.com>
2024-06-12 01:55:33 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,49 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Global n (for memoization)
-int n;
 
-// Iterative fibonacci function
 
-int iter_fib(int n)
+int memo_fib(int n, vector<int>& F)
 {
-    int t0=0 , t1=1, i, sum;
     
-    // if n = 0 or 1 
-    if (n <= 1)
-        return n;
-        
-    // for n > 1
-    for (i = 2; i<=n; i++)
-    {
-        // t2 = sum of previous elements
-        sum = t0 + t1;
-        t0= t1;
-        t1= sum;
-    }
-    return sum;
-}
-
-// Recursive fibonacci function
-
-int rec_fib(int n)
-{
-    if (n<=1)
-        return n;
-        
-    return rec_fib(n-2) + rec_fib(n-1);
-}
-
-// recursive fibonacci using MEMOIZATION
-
-// declaring a global array F with all -1, to store intermediate results & prevent excessive recursion
-// vector<int> F(n,-1); (not working for n declared as Global var??)
-
-int memo_fib(int n)
-{
-    // or declaring vector as static
-    static vector<int> F(n,-1);
     
     if (n <= 1)
         {
@@ -54,11 +16,12 @@ int memo_fib(int n)
     else
     {
         if (F[n-2]==-1)
-            F[n-2] = memo_fib(n-2);
+            F[n-2] = memo_fib(n-2,F);
             
         if (F[n-1]==-1)
-            F[n-1] = memo_fib(n-1);
-            
+            F[n-1] = memo_fib(n-1,F);
+           
+        F[n] = F[n-2] + F[n-1];
         return F[n-2] + F[n-1];
     }
 }
@@ -67,14 +30,15 @@ int memo_fib(int n)
 
 int main()
 {
+    int n;
     cout<<"Enter the value of n: ";
     cin>>n;
 
+    vector<int> F(n,-1);
     //results
-    
-    std::cout<<endl<<"Iterative fibonacci value is : "<<iter_fib(n)<<endl;
-    std::cout<<"Recursive fibonacci value is : "<<rec_fib(n)<<endl;
-    std::cout<<"Recursive fibonacci value using Memoization is : "<<memo_fib(n)<<endl;
+   
+    int ans = memo_fib(n,F);
+    std::cout<<"Recursive fibonacci value using Memoization is : "<<ans<<endl;
 
     return 0;
 }
